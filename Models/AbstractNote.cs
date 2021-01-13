6fd0a5654ff9f392f50188a741b6d052f00df23f
MrWalshyType2 <mwalsh@academytrainee.com>
2021-01-13 15:54:46 +0000

    Added Note model, closes #2

---
+++ Models/AbstractNote.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Noting.Models
+{
+    public abstract class AbstractNote<T>
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public ICollection<string> Keywords { get; set; }
+        public string Topic { get; set; }
+        public string Subtopic { get; set; }
+        public SpacedRepetitionHistory SpacedRepetitionHistory { get; set; }
+        public bool AutomaticIdLinking { get; set; }
+        public ICollection<string> LinkedNoteIds { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime LastModified { get; set; }
+    }
+}
---
+++ Models/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Noting.Models
+{
+    public class Note : AbstractNote<Note>
+    {
+        public void Log()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+}
