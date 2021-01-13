67f604fed11d9ada198abbd5e711ea9cb4e88068
MrWalshyType2 <mwalsh@academytrainee.com>
2021-01-13 16:21:21 +0000

    Registered db context

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Noting.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,9 @@ namespace Noting
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcNoteContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MvcItemContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
