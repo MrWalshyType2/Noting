9d08aef73d34dcff704ac3c9a0a9fcad2d8c4e73
MrWalshyType2 <mwalsh@academytrainee.com>
2021-01-13 15:59:52 +0000

    Created db context class for Note, closes #4

---
+++ Data/MvcNoteContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Noting.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Noting.Data
+{
+    public class MvcNoteContext : DbContext
+    {
+        public MvcNoteContext(DbContextOptions<MvcNoteContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Note> Note { get; set; }
+    }
+}
