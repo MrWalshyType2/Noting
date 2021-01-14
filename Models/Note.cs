8da1986e27f70a13c0d0d9fc1e8ba2fbc34a0866
MrWalshyType2 <mwalsh@academytrainee.com>
2021-01-14 14:41:40 +0000

    Removed abstract note

--- Models/Note.cs
+++ Models/Note.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Noting.Models
 {
-    public class Note : AbstractNote<Note>
+    public class Note
     {
-        public void Log()
-        {
-            Console.WriteLine(ToString());
-        }
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        //public ICollection<Keyword> Keywords { get; set; }
+        public string Topic { get; set; }
+        public string Subtopic { get; set; }
+        //public SpacedRepetitionHistory SpacedRepetitionHistory { get; set; }
+        public bool AutomaticIdLinking { get; set; }
+        public ICollection<Note> LinkedNotes { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime LastModified { get; set; }
     }
 }
