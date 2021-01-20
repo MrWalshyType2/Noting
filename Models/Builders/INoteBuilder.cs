2da62e73577050dd77c446a52d91d01bacd107a2
MrWalshyType2 <mwalsh@academytrainee.com>
2021-01-20 14:45:44 +0000

    Added NoteBuilder

---
+++ Models/Builders/INoteBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Noting.Models.Builders
+{
+    public interface INoteBuilder
+    {
+        INoteBuilder WithId(string id);
+        INoteBuilder WithTitle(string title);
+        INoteBuilder WithDescription(string description);
+        INoteBuilder WithKeywords(ICollection<Keyword> keywords);
+        INoteBuilder WithTopic(string topic);
+        INoteBuilder WithSubtopic(string subtopic);
+        INoteBuilder WithSpacedRepetitionHistory(SpacedRepetitionHistory spacedRepetitionHistory);
+        INoteBuilder HasAutomaticIdLinking(bool automaticIdLinking);
+        INoteBuilder DateCreatedAt(DateTime dateTime);
+        INoteBuilder DateLastModified(DateTime dateTime);
+        INoteBuilder HasChildren(ICollection<NoteRelation> children);
+        Note BuildNote();
+    }
+}
---
+++ Models/Builders/NoteBuilder.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Noting.Models.Builders
+{
+    public class NoteBuilder : INoteBuilder
+    {
+        private string _id;
+        private string _title;
+        private string _description;
+        private ICollection<Keyword> _keywords;
+        private string _topic;
+        private string _subtopic;
+        private SpacedRepetitionHistory _spacedRepetitionHistory;
+        private bool _automaticIdLinking;
+        private DateTime _createdAt;
+        private DateTime _lastModified;
+        private ICollection<NoteRelation> _children;
+
+        public Note BuildNote()
+        {
+            return new Note
+            {
+                Id = _id,
+                Title = _title,
+                Description = _description,
+                Keywords = _keywords,
+                Topic = _topic,
+                Subtopic = _subtopic,
+                SpacedRepetitionHistory = _spacedRepetitionHistory,
+                AutomaticIdLinking = _automaticIdLinking,
+                CreatedAt = _createdAt,
+                LastModified = _lastModified,
+                Children = _children
+            };
+        }
+
+        public INoteBuilder DateCreatedAt(DateTime dateTime)
+        {
+            _createdAt = dateTime;
+            return this;
+        }
+
+        public INoteBuilder HasAutomaticIdLinking(bool automaticIdLinking)
+        {
+            _automaticIdLinking = automaticIdLinking;
+            return this;
+        }
+
+        public INoteBuilder DateLastModified(DateTime dateTime)
+        {
+            _lastModified = dateTime;
+            return this;
+        }
+
+        public INoteBuilder WithDescription(string description)
+        {
+            _description = description;
+            return this;
+        }
+
+        public INoteBuilder WithId(string id)
+        {
+            _id = id;
+            return this;
+        }
+
+        public INoteBuilder WithKeywords(ICollection<Keyword> keywords)
+        {
+            _keywords = keywords;
+            return this;
+        }
+
+        public INoteBuilder WithSpacedRepetitionHistory(SpacedRepetitionHistory spacedRepetitionHistory)
+        {
+            _spacedRepetitionHistory = spacedRepetitionHistory;
+            return this;
+        }
+
+        public INoteBuilder WithSubtopic(string subtopic)
+        {
+            _subtopic = subtopic;
+            return this;
+        }
+
+        public INoteBuilder WithTitle(string title)
+        {
+            _title = title;
+            return this;
+        }
+
+        public INoteBuilder WithTopic(string topic)
+        {
+            _topic = topic;
+            return this;
+        }
+
+        public INoteBuilder HasChildren(ICollection<NoteRelation> children)
+        {
+            _children = children;
+            return this;
+        }
+
+        public static implicit operator Note(NoteBuilder builder)
+        {
+            return builder.BuildNote();
+        }
+    }
+}
