f8745260b2545cccec4d93188183cc5e5e1aa944
MrWalshyType2 <mwalsh@academytrainee.com>
2021-01-20 17:20:39 +0000

    Added note service

---
+++ Services/INoteService.cs
@@ -0,0 +1,19 @@
+﻿using Noting.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Noting.Services
+{
+    interface INoteService
+    {
+        public Task GetAll();
+        public Task GetDetails(string id);
+        public Task CreateNote(NotePageViewModel model);
+        public Task CreateNote(Note model);
+        public Task EditNote(string id, NotePageViewModel model);
+        public Task EditNote(string id, Note model);
+        public Task DeleteNote(string id);
+    }
+}
